d52e2f2089db0e9fc730f1e972ee9f58899e9d1a
adm <kaio.candf@gmail.com>
2026-05-06 16:30:11 -0300

    enviando codigo inicial

---
+++ exemplo.php
@@ -0,0 +1,29 @@
+<?php
+    $footer = "@ 2026 Curso de PHP";
+    $section = "teeeeeeeeeeestetetssssssss";
+    $header = "Curso de PHP";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header> 
+        <h1><?php echo $header ?></h1>
+    </header>
+
+    <section>
+        <p><?php echo $section ?></p>
+    </section>
+
+    <footer>
+        <p><?php echo $footer ?></p>
+    </footer>
+    
+</body>
+</html>
